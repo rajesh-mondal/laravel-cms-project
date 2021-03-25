53f89b3692cc97bb187299268cb3157579515136
Rajesh Mondal <rajesh01749@gmail.com>
2021-03-25 22:53:09 +0600

    working with controller and route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/shout', [HomeController::class,'shoutHome'])->name('shout');
